b422863566cf519f99bbb864e93f1c9f1824aaba
dark452 <dark452@gmail.com>
2023-12-07 02:19:40 +0000

    Initial commit, working version

---
+++ challenge_1/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.11.0"
+    }
+  }
+}
+
+provider "kubernetes" {
+  # Configuration options
+  config_path = "/root/.kube/config"
+}
+
